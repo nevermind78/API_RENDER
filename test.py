10ac49719afe3aab55fc36f1be514ea3d492a410
NEVERMIND78 <abdallah.khemais@gmail.com>
2025-03-05 04:10:32 +0100

    last

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ url = 'https://api-render-146a.onrender.com/predict'
 
 # Chemin vers l'image à tester
 #image_path = 'test_image_1.png'
-image_path="C:/Users/abdal/OneDrive/Bureau/master-master/labs/Project/test_images/test_image_5.png"
+image_path="test_images/test_image_10.png"
 # Charger l'image et l'envoyer avec le modèle choisi
 with open(image_path, 'rb') as file:
     files = {'file': file}
