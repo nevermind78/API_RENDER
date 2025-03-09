c6e89619c9291f833cbe17d531c889db493ef93d
NEVERMIND78 <abdallah.khemais@gmail.com>
2025-03-09 02:37:42 +0100

    vercel

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ import requests
 url = 'https://api-render-146a.onrender.com/predict'
 
 # Chemin vers l'image à tester
-#image_path = 'test_image_1.png'
-image_path="test_images/test_image_10.png"
+#image_path = 'test_image_11.png'
+image_path="test_images/test_image_11.png"
 # Charger l'image et l'envoyer avec le modèle choisi
 with open(image_path, 'rb') as file:
     files = {'file': file}
